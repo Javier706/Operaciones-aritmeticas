498f629d86e668aa24c41bbdd72557833b153899
Javier706 <103207142+Javier706@users.noreply.github.com>
2022-04-07 21:12:45 -0400

    Delete Suma.py

--- Suma.py
+++
@@ -1,5 +0,0 @@
-print ("Programa de suma")
-valor1 = int(input("Digite un numero"))
-valor2 =  int(input("Digite otro numero"))
-resultado = valor1 + valor2
-print (f"El resultado de la suma es: {resultado}")
